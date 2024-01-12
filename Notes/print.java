75761ee1503a2c8920b0bfea1c6ac6bfcf0f814e
Yasemin Ibryam <yaseminibr123@gmail.com>
2024-01-12 09:38:21 +0200

    new repo

---
+++ Notes/print.java
@@ -0,0 +1,14 @@
+package Notes;
+
+public class print {
+    public static void main(String[] args) {
+        System.out.println("Hello world!"); // print something and goes to the next line
+        // shortcut - sout
+
+
+        System.out.printf("%s, %d, %f%n", "hello", 15, 1.0); // print and format, doesn't go to the next line(we have to put %n to do it)
+        //shortcut - souf
+
+        System.out.print("Hello word!"); // just print and stay in the same line
+    }
+}
